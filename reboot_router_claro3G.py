258c24c86ebbcc4a4a347e916d520c0f98f82f90
Cleiton Bueno <cleitonrbueno@gmail.com>
2015-12-14 21:08:44 -0200

    Add variables with info access router

--- reboot_router_claro3G.py
+++ reboot_router_claro3G.py
@@ -3,10 +3,22 @@
 
 import urllib2 as http
 
-# URL with GET to reboot router or status main page to tests
-#url_get_reboot = 'http://10.11.12.254/log/in?un=admin&pw=admin12&rd=%2Fuir%2Frebo.htm?rc=&Nrd=0&Nsm=1'
-url_get_status = 'http://10.11.12.254/log/in?un=admin&pw=admin12&rd=%2Fuir%2Fstatus.htm&rd2=%2Fuir%2Fwanst.htm&Nrd=1'
-url_root = url_get_status
+
+# Data Router
+user_router = "user_here"
+pass_router = "password_here"
+ip_router   = "IP_here"
+port_router = "80"
+
+
+# URL with filling the fields above, URL with GET to reboot router or status main page to tests
+url_get_reboot = "http://" + ip_router + ":" + port_router + "/log/in?un=" + user_router + "&pw=" + pass_router + "&rd=%2Fuir%2Frebo.htm?rc=&Nrd=0&Nsm=1"
+#url_get_status = "http://" + ip_router + ":" + port_router + "/log/in?un=" + user_router + "&pw=" + pass_router + "&rd=%2Fuir%2Fstatus.htm&rd2=%2Fuir%2Fwanst.htm&Nrd=1"
+
+# Variable global to open URL
+url_root = url_get_reboot
+
+print "Processing URL: %s" % url_root
 
 # Handling HTTP Cookie - Session Cookie Router
 cookieprocessor = http.HTTPCookieProcessor()
