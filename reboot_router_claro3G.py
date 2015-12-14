0c7c19a9edffea9474b0aa6379bafef283425483
Cleiton Bueno <cleitonrbueno@gmail.com>
2015-12-14 20:56:55 -0200

    Add variavel url_root to url of the reboot router and status mainpage router

--- reboot_router_claro3G.py
+++ reboot_router_claro3G.py
@@ -3,9 +3,10 @@
 
 import urllib2 as http
 
-# URL with GET to reboot router
-url_get_reboot = 'http://10.11.12.254/log/in?un=admin&pw=admin12&rd=%2Fuir%2Frebo.htm?rc=&Nrd=0&Nsm=1'
-
+# URL with GET to reboot router or status main page to tests
+#url_get_reboot = 'http://10.11.12.254/log/in?un=admin&pw=admin12&rd=%2Fuir%2Frebo.htm?rc=&Nrd=0&Nsm=1'
+url_get_status = 'http://10.11.12.254/log/in?un=admin&pw=admin12&rd=%2Fuir%2Fstatus.htm&rd2=%2Fuir%2Fwanst.htm&Nrd=1'
+url_root = url_get_status
 
 # Handling HTTP Cookie - Session Cookie Router
 cookieprocessor = http.HTTPCookieProcessor()
@@ -17,7 +18,7 @@ opener = http.build_opener(cookieprocessor)
 http.install_opener(opener)
 
 # Open URL with Opener above
-payload_router = http.urlopen(url_get_reboot)
+payload_router = http.urlopen(url_root)
 
 # Print payload Request URL
 print "Payload %s" % payload_router.read()
