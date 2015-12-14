97a6cf18f22974889e5b7b75332e1f124b646b81
Cleiton Bueno <cleitonrbueno@gmail.com>
2015-12-14 20:53:20 -0200

    Import mod urllib2, using CookieProcessor and open URL to reboot router.

--- reboot_router_claro3G.py
+++ reboot_router_claro3G.py
@@ -1,2 +1,23 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
+
+import urllib2 as http
+
+# URL with GET to reboot router
+url_get_reboot = 'http://10.11.12.254/log/in?un=admin&pw=admin12&rd=%2Fuir%2Frebo.htm?rc=&Nrd=0&Nsm=1'
+
+
+# Handling HTTP Cookie - Session Cookie Router
+cookieprocessor = http.HTTPCookieProcessor()
+
+# Customize it Opener with CookieProcessor
+opener = http.build_opener(cookieprocessor)
+
+# Using here Opener + CookieProcessor
+http.install_opener(opener)
+
+# Open URL with Opener above
+payload_router = http.urlopen(url_get_reboot)
+
+# Print payload Request URL
+print "Payload %s" % payload_router.read()
